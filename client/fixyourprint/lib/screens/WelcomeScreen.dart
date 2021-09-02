aa772412fd7ad875bbeefe14c72c9940880fac1a
hrigved-dev <hrigved.khatavkar@gmail.com>
2021-09-02 10:51:25 +0530

    Tokenization and Logout Logic Complete

--- client/fixyourprint/lib/screens/WelcomeScreen.dart
+++ client/fixyourprint/lib/screens/WelcomeScreen.dart
@@ -1,15 +1,56 @@
+import 'package:fixyourprint/services/AuthService.dart';
+import 'package:fixyourprint/widgets/GreenLoader.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class WelcomeScreen extends StatelessWidget {
+class WelcomeScreen extends StatefulWidget {
   final String token;
   WelcomeScreen({Key? key, required this.token}) : super(key: key);
 
   @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _isLoading = false;
+  }
+
+  logoutUser() async {
+    setState(() {
+      _isLoading = true;
+    });
+    AuthService().logoutUser().then((value) {
+      if (value == '') {
+        _isLoading = false;
+        Navigator.pop(context);
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('WelcomeScreen and token is ' + token),
-      ),
+      body: _isLoading
+          ? GreenLoader()
+          : Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text('WelcomeScreen and token is ' + widget.token),
+                  MaterialButton(
+                      color: Colors.black,
+                      onPressed: () {
+                        logoutUser();
+                      })
+                ],
+              ),
+            ),
     );
   }
 }
--- client/fixyourprint/lib/services/AuthService.dart
+++ client/fixyourprint/lib/services/AuthService.dart
@@ -1,22 +1,36 @@
-import 'dart:convert';
-
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:dio/dio.dart';
-import 'package:fixyourprint/models/UserModel.dart';
-import 'package:http/http.dart' as http;
 
 class AuthService {
   Dio dio = new Dio();
 
-  // getUsers() async {
-  //   var response = await dio.get('http://10.0.2.2:3000/users');
-  //   print(response.data.toString());
-  // }
+  getUsers() async {
+    var response = await dio.get('http://10.0.2.2:3000/users');
+    print(response.data.toString());
+  }
 
   loginUser(String email, String password) async {
     try {
       var response = await dio.post(
         'http://10.0.2.2:3000/users/login',
         data: {"email": email, "password": password},
+        // options: Options(contentType: Headers.formUrlEncodedContentType)
+      );
+      if (response.statusCode == 200) {
+        SharedPreferences preferences = await SharedPreferences.getInstance();
+        await preferences.setString('token', response.data['token']);
+        return response.data['token'];
+      } else {
+        print(response.data['error']);
+      }
+    } on DioError catch (e) {}
+  }
+
+  createUser(email, name, password) async {
+    try {
+      var response = await dio.post(
+        'http://10.0.2.2:3000/users',
+        data: {"name": name, "email": email, "password": password},
         // options: Options(contentType: Headers.formUrlEncodedContentType)
       );
       if (response.statusCode == 200) {
@@ -27,15 +41,17 @@ class AuthService {
     } on DioError catch (e) {}
   }
 
-  createUser(email, name, password) async {
+  logoutUser() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    var token = preferences.getString('token');
+    dio.options.headers['authorization'] = "Bearer $token";
     try {
-      return await dio.post(
-        'http://10.2.2:3000/users',
-        data: {"name": name, "email": email, "password": password},
-        // options: Options(contentType: Headers.formUrlEncodedContentType)
-      );
+      var response = await dio.post('http://10.0.2.2:3000/users/logout');
+      if (response.statusCode == 200) {
+        return response.data;
+      }
     } on DioError catch (e) {
-      print(e.response);
+      print('Error');
     }
   }
 }
