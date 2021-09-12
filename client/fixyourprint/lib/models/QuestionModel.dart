80fe49ac1ed6fc0e8c3667e49eecb03af020b25f
hrigved-dev <hrigved.khatavkar@gmail.com>
2021-09-12 18:37:00 +0530

    Dynamic Slider

--- client/fixyourprint/lib/models/QuestionModel.dart
+++ client/fixyourprint/lib/models/QuestionModel.dart
@@ -1,12 +1,16 @@
-import 'package:fixyourprint/services/CarbonDataService.dart';
-
 class QuestionModel {
   late String question;
   late String parameter;
   late String lottieUrl;
+  late double min;
+  late double max;
+  late int divisions;
 
   QuestionModel(
       {required this.question,
       required this.parameter,
-      required this.lottieUrl});
+      required this.lottieUrl,
+      required this.min,
+      required this.max,
+      required this.divisions});
 }
