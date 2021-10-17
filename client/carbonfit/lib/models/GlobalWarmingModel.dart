31a8c8aeff4a31a162efada11c4b67bfc752449d
hrigved-dev <hrigved.khatavkar@gmail.com>
2021-10-17 19:43:02 +0530

    Carbon Info

--- client/carbonfit/lib/models/GlobalWarmingModel.dart
+++ client/carbonfit/lib/models/GlobalWarmingModel.dart
@@ -1,6 +1,14 @@
 class GlobalWarmingModel {
   late String year;
+  late String month;
+  late String day;
+  late String cycle;
   late String trend;
 
-  GlobalWarmingModel({required this.year, required this.trend});
+  GlobalWarmingModel(
+      {required this.year,
+      required this.trend,
+      required this.cycle,
+      required this.day,
+      required this.month});
 }
--- client/carbonfit/lib/services/GlobalWarmingService.dart
+++ client/carbonfit/lib/services/GlobalWarmingService.dart
@@ -1,17 +1,25 @@
-// import 'package:dio/dio.dart';
-// import 'package:fixyourprint/models/GlobalWarmingModel.dart';
+import 'package:dio/dio.dart';
+import 'package:fixyourprint/models/GlobalWarmingModel.dart';
 
-// class GlobalWarmingService {
-//   Dio dio = new Dio();
-//   var baseURL = "https://global-warming.org/api/co2-api#";
-//   List<GlobalWarmingModel> globalWarming = [];
+class GlobalWarmingService {
+  Dio dio = new Dio();
+  var baseURL = "https://global-warming.org/api/co2-api#";
+  List<GlobalWarmingModel> globalWarming = [];
 
-//   Future<void> getData() async {
-//     var response = await dio.get('$baseURL');
-//     var data = response.data['co2'];
+  Future<void> getData() async {
+    var response = await dio.get('$baseURL');
+    var data = response.data['co2'];
 
-//     if (response.statusCode == 200) {
-//       for (int i = 0; i < data.length; i = i++) {}
-//     }
-//   }
-// }
+    if (response.statusCode == 200) {
+      for (int i = 0; i < data.length; i++) {
+        GlobalWarmingModel globalWarmingModel = GlobalWarmingModel(
+            year: data[i]["year"],
+            trend: data[i]["trend"],
+            cycle: data[i]["cycle"],
+            day: data[i]["day"],
+            month: data[i]["month"]);
+        globalWarming.add(globalWarmingModel);
+      }
+    }
+  }
+}
